9f92eefd0c48f32a582a1acf65c36fef9b588ccd
santilagos <sntlgs@gmail.com>
2023-08-01 17:39:37 -0300

    projects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,7 +1,24 @@
-
+import { dataProjects } from "../data/dataProjects"
 const Projects = () => {
   return (
-    <div>Projects</div>
+    <div name='projects'>
+      {dataProjects.map((project) => {
+        return(
+        <div>
+          <h1>{project.name}</h1>
+          <p>{project.description}</p>
+          <a target="_blank" href={project.deploy}>
+            <button>Deploy</button>
+          </a>
+          <a target="_blank"  href={project.youtube}>
+            <button>Overview</button>
+          </a>
+          <a target="_blank"  href={project.repository}>
+            <button>Repository</button>
+          </a>
+        </div>)
+      })}
+    </div>
   )
 }
 
