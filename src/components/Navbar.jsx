889a119ff4f9858e08b016626c7c2963f7894087
santilagos <sntlgs@gmail.com>
2023-08-01 16:27:05 -0300

    about me

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
-
+import { Link } from 'react-scroll';
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <nav>
+      <ul>
+        <li><Link to="about" smooth={true} duration={500}>About</Link></li>
+        <li><Link to='projects' smooth={true} duration={500}>Proyects</Link></li>
+        <li><Link to='skills' smooth={true} duration={500}>Tech skills</Link></li>
+        <li><Link to='contact' smooth={true} duration={500}>Contact</Link></li>
+      </ul>
+    </nav> 
   )
 }
 
--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -1,7 +1,15 @@
-
+import {name} from '../data/dataContact'
+import {aboutMe} from '../data/dataAbout'
+import {Element} from 'react-scroll'
 const About = () => {
   return (
-    <div>About</div>
+    <Element name='about'>
+      <div >
+      <p>Hi im {name}</p>
+      <p>{aboutMe}</p>
+    </div>
+    </Element>
+    
   )
 }
 
