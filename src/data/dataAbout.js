3efae066986fcfbaf86ec6fe1cabba0892e88ddf
santilagos <sntlgs@gmail.com>
2023-08-01 15:40:44 -0300

    structure

---
+++ src/data/dataAbout.js
@@ -0,0 +1,3 @@
+const name = 'Santiago Rovaletti'
+const aboutMe = '¡Hola! Soy Santiago Rovaletti, un apasionado desarrollador web fullstack de 22 años de edad, originario de Argentina y actualmente residiendo en CABA. Mi enfoque principal se encuentra en el desarrollo backend, donde disfruto diseñar y construir sólidas arquitecturas de servidor para aplicaciones web. Me encanta enfrentar desafíos técnicos y estoy constantemente buscando aprender nuevas tecnologías y mejorar mis habilidades. Además, soy un entusiasta del trabajo en equipo y siempre estoy dispuesto a colaborar en proyectos que impulsen la innovación tecnológica.'
+
