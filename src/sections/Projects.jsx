cb3609951794c21e4b74d2def9d29839ccaa17dd
gastondvoskin <dvoskingaston@gmail.com>
2023-08-02 11:50:51 -0300

    Restructured data (WIP) and executed Prettier

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,25 +1,27 @@
-import { dataProjects } from "../data/dataProjects"
+import { dataProjects } from "../data/dataRovaletti/dataProjects";
+
 const Projects = () => {
   return (
-    <div name='projects'>
+    <div name="projects">
       {dataProjects.map((project) => {
-        return(
-        <div>
-          <h1>{project.name}</h1>
-          <p>{project.description}</p>
-          <a target="_blank" href={project.deploy}>
-            <button>Deploy</button>
-          </a>
-          <a target="_blank"  href={project.youtube}>
-            <button>Overview</button>
-          </a>
-          <a target="_blank"  href={project.repository}>
-            <button>Repository</button>
-          </a>
-        </div>)
+        return (
+          <div>
+            <h1>{project.name}</h1>
+            <p>{project.description}</p>
+            <a target="_blank" href={project.deploy}>
+              <button>Deploy</button>
+            </a>
+            <a target="_blank" href={project.youtube}>
+              <button>Overview</button>
+            </a>
+            <a target="_blank" href={project.repository}>
+              <button>Repository</button>
+            </a>
+          </div>
+        );
       })}
     </div>
-  )
-}
+  );
+};
 
-export default Projects
+export default Projects;
