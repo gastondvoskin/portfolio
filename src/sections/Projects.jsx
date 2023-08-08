2ee521760ca421154d87bda9b311abdca82bd89f
santilagos <sntlgs@gmail.com>
2023-08-08 13:55:01 -0300

    add pop up detail proyects

--- src/sections/Projects.jsx
+++ src/sections/Projects.jsx
@@ -1,39 +1,50 @@
 import { dataDvoskinProjects } from "../data/dataDvoskin/dataProjects"; 
 import { dataRovalettiProjects } from "../data/dataRovaletti/dataProjects";
+import ProjectDetailModal from "../components/ProjectDetailModal";
+import { useState } from "react";
 
 const Projects = () => {
   const dataOwner = import.meta.env.VITE_DATAOWNER;
   const dataProjects = dataOwner === 'dvoskin' ? dataDvoskinProjects : dataRovalettiProjects;
 
+  const [selectedProject, setSelectedProject] = useState(null);
+
+  const openProjectDetail = (project) => {
+    setSelectedProject(project);
+  };
+
+  const closeProjectDetail = () => {
+    setSelectedProject(null);
+  };
+
   return (
     <main name="projects" className="flex flex-col w-full items-center pt-16 pb-16 bg-mybg2 dark:bg-mybg2d">
       <div className="flex flex-col items-center w-3/4">
         <h1 className="text-4xl text-myacc dark:text-myaccd">PROJECTS</h1>
-        {dataProjects.map((project) => {
+        {dataProjects.map((project, index) => {
           return (
-            <div className="grid grid-cols-2 m-10">  {/* mapped div in grid */}
-              <img className="w-3/4" src={project.image} alt="" />  {/* left */}
-              <div className="flex flex-col items-center"> {/* right */}
+            <div key={index} className="grid grid-cols-2 m-10">
+              <img className="w-3/4" src={project.image} alt="" />
+              <div className="flex flex-col items-center">
                 <h2 className="text-2xl">{project.name}</h2>
                 <p className="text-base">{project.description}</p>
-
-                {/* buttons */}
-                <div className="flex">
-                  <a target="_blank" href={project.deploy}>
-                    <button className="m-3 p-1 border-solid border-2 border-black rounded-md bg-myacc text-white dark:bg-myaccd dark:text-black">Deploy</button>
-                  </a>
-                  <a target="_blank" href={project.youtube}>
-                    <button className="m-3 p-1 border-solid border-2 border-black rounded-md bg-myacc text-white dark:bg-myaccd dark:text-black">Overview</button>
-                  </a>
-                  <a target="_blank" href={project.repository}>
-                    <button className="m-3 p-1 border-solid border-2 border-black rounded-md bg-myacc text-white dark:bg-myaccd dark:text-black">Repository</button>
-                  </a>
-                </div>
+                <button
+                  className="m-3 p-1 border-solid border-2 border-black rounded-md bg-myacc text-white dark:bg-myaccd dark:text-black"
+                  onClick={() => openProjectDetail(project)}
+                >
+                  View Detail
+                </button>
               </div>
             </div>
           );
         })}
       </div>
+      {selectedProject && (
+        <ProjectDetailModal
+          project={selectedProject}
+          onClose={closeProjectDetail}
+        />
+      )}
     </main>
   );
 };
