72d2f25d1ba36457e7156d96be231878d7af8017
gastondvoskin <dvoskingaston@gmail.com>
2023-08-10 08:28:15 -0300

    Year updated dynamically

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
-
 const Footer = () => {
   const dataOwner = import.meta.env.VITE_DATAOWNER;
   const authors = dataOwner === 'dvoskin' ? "Gastón Dvoskin & Santiago Rovaletti" : "Santiago Rovaletti & Gastón Dvoskin";
+
+  const currentYear = new Date().getFullYear();
 
   return (
     <footer 
       name="footer" 
       className="flex flex-col w-full justify-center items-center h-32 bg-white text-black border-t-4 border-solid border-black dark:bg-slate-900 dark:text-white">
-        <h1>{`© 2023 ${authors}. All Rights Reserved.`}</h1> 
+        <h1>{`© ${currentYear} ${authors}. All Rights Reserved.`}</h1> 
     </footer>
   );
 };
