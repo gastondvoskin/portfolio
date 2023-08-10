e323f04f7b66b1a312c415bd7021b5962bff835d
santilagos <sntlgs@gmail.com>
2023-08-10 10:48:56 -0300

    first structure

---
+++ api/src/app.js
@@ -0,0 +1,35 @@
+const express = require("express");
+const morgan = require("morgan");
+const cors = require("cors");
+
+
+const app = express();
+
+app.use(express.json());
+app.use(morgan("dev"));
+
+app.use(cors());
+app.use((req, res, next) => {
+    res.header('Access-Control-Allow-Origin', '*'); /* https://viandaexpress.vercel.app, http://localhost:5173 */
+    res.header("Access-Control-Allow-Credentials", "true");
+    res.header(
+      "Access-Control-Allow-Headers",
+      "Origin, X-Requested-With, Content-Type, Accept"
+    );
+    res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+    next();
+  });
+app.use((req,res,next)=>{
+    console.log('hola que haceeee');
+    next()
+})
+  
+app.use((err, req, res, next) => {
+    /* ?? */
+    const status = err.status || 500;
+    const message = err.message || err;
+    console.error(err);
+    res.status(status).send(message);
+  });
+  
+  module.exports = app;
