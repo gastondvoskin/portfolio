767fe07ff1161ab8d425444a1d2a016c40def942
gastondvoskin <dvoskingaston@gmail.com>
2023-08-10 08:25:18 -0300

    Updated and styled Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,14 @@
-import { dataDvoskinContact } from "../data/dataDvoskin/dataContact";
-import { dataRovalettiContact } from "../data/dataRovaletti/dataContact";
 
 const Footer = () => {
   const dataOwner = import.meta.env.VITE_DATAOWNER;
-  const dataContact = dataOwner ===  "dvoskin" ? dataDvoskinContact : dataRovalettiContact
+  const authors = dataOwner === 'dvoskin' ? "Gastón Dvoskin & Santiago Rovaletti" : "Santiago Rovaletti & Gastón Dvoskin";
+
   return (
-    <main name="footer" className="flex w-full justify-center h-50 items-center bg-white text-mytextnav border-t-4 border-solid border-black dark:bg-slate-900 dark:text-white">
-      <div name="footer" className="flex flex-col w-full justify-center items-center bg-white text-mytextnav border-t-4 border-solid border-black dark:bg-slate-900 dark:text-white">
-      </div>
-        {dataOwner === "dvoskin" ? <h1>Developed by Gastón Dvoskin & Santiago Rovaletti</h1> : <h1>Developed by Santiago Rovaletti & Gastón Dvoskin</h1>}
-    </main>
+    <footer 
+      name="footer" 
+      className="flex flex-col w-full justify-center items-center h-32 bg-white text-black border-t-4 border-solid border-black dark:bg-slate-900 dark:text-white">
+        <h1>{`© 2023 ${authors}. All Rights Reserved.`}</h1> 
+    </footer>
   );
 };
 
