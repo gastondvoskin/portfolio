672000afe7f6602af9cf0b5b330cb2bac41e477a
gastondvoskin <dvoskingaston@gmail.com>
2023-08-12 12:54:55 -0300

    Home responsive

--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -8,31 +8,28 @@ const Home = () => {
   const name = dataOwner === "dvoskin" ? nameDvoskin : nameRovaletti;
 
   return (
-    <main
+    <section
       name="home"
-      className="flex flex-col w-full h-screen items-center pt-24 pb-24 bg-mybg1 dark:bg-mybg1d"
+      className="w-full h-screen pt-24 pb-24 bg-mybg1 dark:bg-mybg1d"
     >
       <div className="flex flex-col w-full h-full justify-center items-center ">
         {/* <h1 className="text-8xl pt-24 text-orange-800 m-60 text-center">PORTFOLIO UNDER CONSTRUCTION</h1> */}
         
-        <h3 className="mb-8 text-5xl mt-32">
+        <h3 className="text-md mb-10 md:text-5xl md:mb-16 xl:mt-16 ">
           Welcome! My name is
         </h3>
-        <h1 className="mb-1 text-9xl text- text-myacc dark:text-myaccd">{name}</h1>
-        <h2 className="mb-32 text-7xl">Full-stack web developer</h2>
+        <h1 className={`${dataOwner==="dvoskin" ? "text-4xl" : "text-3xl"} mb-2 text-myacc dark:text-myaccd md:text-8xl xl:text-9xl`}>{name}</h1>
+        <h2 className="text-xl mb-32 md:text-4xl xl:text-6xl">Full-stack web developer</h2>
         <Link
-          className="px-7 cursor-pointer text-3xl text-myacc dark:text-myaccd"
+          className="text-md px-7 cursor-pointer text-myacc dark:text-myaccd md:text-xl lg:text-3xl xl:text-4xl"
           to="about"
           smooth={true}
           duration={300}
         >
           Click to know about me!
         </Link>
-
-
-
       </div>
-    </main>
+    </section>
   );
 };
 
