cc8216aa54ee874d0c5228f20ef2bc84af181238
gastondvoskin <dvoskingaston@gmail.com>
2023-08-12 13:37:30 -0300

    Responsive About

--- src/sections/About.jsx
+++ src/sections/About.jsx
@@ -6,27 +6,30 @@ import cvDvoskin from "../assets/CV/Dvoskin.pdf";
 
 const About = () => {
   const dataOwner = import.meta.env.VITE_DATAOWNER;
-  const dataAbout =
-    dataOwner === "dvoskin" ? dataDvoskinAbout : dataRovalettiAbout;
-  const profileImg =  dataOwner === "dvoskin" ? dvoskin : rovaletti;
+  const dataAbout = dataOwner === "dvoskin" ? dataDvoskinAbout : dataRovalettiAbout;
+  const profileImg = dataOwner === "dvoskin" ? dvoskin : rovaletti;
 
   /* className="grid grid-cols-2 m-10 */
   return (
-    <main
+    <section
       name="about"
       className="flex flex-col w-full items-center pt-24 pb-24 bg-mybg2 dark:bg-mybg2d"
     >
-      <div className="flex flex-col w-3/4 items-center">
-        <h1 className="mb-16 text-5xl font-bold text-myacc dark:text-myaccd">
+      {/* about, image, CV, description */}
+      <div className="flex flex-col w-4/5 items-center justify-center">
+        <h1 className="mb-16 text-3xl font-bold text-myacc dark:text-myaccd md:text-5xl">
           ABOUT
         </h1>
-        <div className="grid grid-cols-2">
-          <div className="flex flex-col items-end mr-16">
-            <img className="w-4/5 mb-16" src={profileImg} alt="Dvoskin" />{" "}
-            {/* editing */}
-            <div className="flex">
+
+        {/* image, CV, description */}
+        <div className="grid grid-cols-1 lg:grid-cols-2 justify-center items-center">
+          {/* image and CV */}
+          <div className="flex flex-col items-center">
+            <img className="w-4/5 mb-8 md:w-1/2" src={profileImg} alt="Dvoskin" />{" "}
+            {/* CV buttons */}
+            <div className="flex flex-col">
               <a
-                className="bg-myacc border-2 border-black text-white rounded-xl p-2 text-l dark:bg-myaccd dark:border-white m-4"
+                className="bg-myacc border-2 border-black text-white rounded-xl p-2 text-base dark:bg-myaccd dark:border-white m-4"
                 href={cvDvoskin}
                 target="_blank"
                 rel="noopener noreferrer"
@@ -34,7 +37,7 @@ const About = () => {
                 ► Open CV
               </a>
               <a
-                className="bg-myacc border-2 border-black text-white rounded-xl p-2 text-l dark:bg-myaccd dark:border-white m-4"
+                className="bg-myacc border-2 border-black text-white rounded-xl p-2 text-base dark:bg-myaccd dark:border-white m-4"
                 href={cvDvoskin}
                 rel="noopener noreferrer"
                 download
@@ -43,7 +46,8 @@ const About = () => {
               </a>
             </div>
           </div>
-          <div className="w-3/4 ml-16">
+          {/* description */}
+          <div className="xl:ml-16">
             {dataAbout.map((paragraph, index) => {
               return (
                 <p key={index} className="font-sans text-base mb-10">
@@ -54,7 +58,7 @@ const About = () => {
           </div>
         </div>
       </div>
-    </main>
+    </section>
   );
 };
 
--- src/sections/Home.jsx
+++ src/sections/Home.jsx
@@ -18,7 +18,7 @@ const Home = () => {
         <h3 className="text-md mb-10 md:text-5xl md:mb-16 xl:mt-16 ">
           Welcome! My name is
         </h3>
-        <h1 className={`${dataOwner==="dvoskin" ? "text-4xl" : "text-3xl"} mb-2 text-myacc dark:text-myaccd md:text-8xl xl:text-9xl`}>{name}</h1>
+        <h1 className={`${dataOwner==="dvoskin" ? "text-4xl" : "text-3xl"} mb-2 text-myacc dark:text-myaccd md:text-7xl xl:text-9xl`}>{name}</h1>
         <h2 className="text-xl mb-32 md:text-4xl xl:text-6xl">Full-stack web developer</h2>
         <Link
           className="text-md px-7 cursor-pointer text-myacc dark:text-myaccd md:text-xl lg:text-3xl xl:text-4xl"
