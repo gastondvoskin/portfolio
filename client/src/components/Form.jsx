4a696a2e3fdfe4bf1b32ebb6df051dafae844a34
gastondvoskin <dvoskingaston@gmail.com>
2023-09-21 10:14:48 -0300

    Styled and modularized Form

--- client/src/components/Form.jsx
+++ client/src/components/Form.jsx
@@ -43,72 +43,16 @@ const Form = () => {
   };
 
   return (
-    <div className="w-full lg:w-1/2">
+    <div className="w-full lg:w-1/2 mb-16">
 			<div className="leading-loose">
 				<form
 					onSubmit={onSubmit}
 					className="max-w-xl m-4 p-6 sm:p-10 bg-mybg2 dark:bg-mybg2d rounded-xl shadow-xl text-left"
 				>
-          <Form_Input name="name" labelText="Full Name" value={inputs.name} onChange={onChange} placeHolder="Your name"/>
-          <div className="mb-4">
-            <label
-              className="block mb-1 text-lg text-black dark:text-white"
-              htmlFor="name"
-            >
-              Full Name
-            </label>
-            <input
-              value={inputs.name}
-              onChange={onChange}
-              className="w-full px-5 py-2 bg-white text-black text-md border border-gray-300 border-opacity-50 rounded-md shadow-sm dark:bg-mybg1d dark:text-white dark:border-gray-500"
-              type="text"
-              id="name"
-              name="name"
-              placeholder="Your name"
-              aria-label="Name"
-              required
-            />
-          </div>
+          <Form_Input Element="input" name="name" labelText="Full Name" value={inputs.name} onChange={onChange} placeholder="Your name"/>
+          <Form_Input Element="input" name="email" labelText="Email" value={inputs.email} onChange={onChange} placeholder="Your email"/>
+          <Form_Input Element="textarea" name="message" labelText="Message" value={inputs.message} onChange={onChange} placeholder="Your message" />
 
-          <div className="font-general-regular mb-4">
-            <label
-              className="block text-lg text-primary-dark dark:text-primary-light mb-1"
-              htmlFor="email"
-            >
-              Email
-            </label>
-            <input
-              value={inputs.email}
-              onChange={onChange}
-              className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
-              type="Email"
-              id="email"
-              name="email"
-              placeholder="Your email"
-              aria-label="Email"
-              required
-            />
-          </div>
-					<div className="mt-6">
-						<label
-							className="block text-lg text-primary-dark dark:text-primary-light mb-2"
-							htmlFor="message"
-						>
-							Message
-						</label>
-						<textarea
-              value={inputs.message}
-              onChange={onChange}
-							className="w-full px-5 py-2 border border-gray-300 dark:border-primary-dark border-opacity-50 text-primary-dark dark:text-secondary-light bg-ternary-light dark:bg-ternary-dark rounded-md shadow-sm text-md"
-							id="message"
-							name="message"
-							cols="14"
-							rows="6"
-							aria-label="Message"
-						>
-              Your message   
-            </textarea>
-					</div>
 
           <div className="font-general-medium w-40 px-4 py-2.5 text-white text-center font-medium tracking-wider bg-indigo-500 hover:bg-indigo-600 focus:ring-1 focus:ring-indigo-900 rounded-lg mt-6 duration-500">
             <button
--- client/src/components/Form_Input.jsx
+++ client/src/components/Form_Input.jsx
@@ -1,23 +1,25 @@
-const Form_Input = ({name, labelText, value, onChange, placeHolder}) => {
+const Form_Input = ({ Element, name, labelText, value, onChange, placeholder }) => {
+  const sharedAttributes = {
+    value,
+    onChange,
+    className: "w-full px-5 py-2 bg-white text-black text-md border border-gray-300 border-opacity-50 rounded-md shadow-sm dark:bg-mybg1d dark:text-white dark:border-gray-500",
+    id: name,
+    name,
+    placeholder,
+    "aria-label": name,
+    required: true,
+  };
+
   return (
-    <div className="mb-4">
-      <label
-        className="block mb-1 text-lg text-black dark:text-white"
-        htmlFor={name}
-      >
+    <div className={`mb-4 ${Element === "textarea" ? "mt-10" : ""}`}>
+      <label className="mb-1 text-lg text-black dark:text-white" htmlFor={name}>
         {labelText}
       </label>
-      <input
-        value={value}
-        onChange={onChange}
-        className="w-full px-5 py-2 bg-white text-black text-md border border-gray-300 border-opacity-50 rounded-md shadow-sm dark:bg-mybg1d dark:text-white dark:border-gray-500"
-        type="text"
-        id={name}
-        name={name}
-        placeholder={placeHolder}
-        aria-label={name}
-        required
-      />
+      {Element === "input" ? (
+        <input {...sharedAttributes} type="text" />
+      ) : (
+        <textarea {...sharedAttributes} rows={5} />
+      )}
     </div>
   );
 };
