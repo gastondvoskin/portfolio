a005ed3269e776d4c95673fdee6e6a327f044487
gastondvoskin <dvoskingaston@gmail.com>
2023-09-22 16:21:22 -0300

    Modularized Contac_Icon

--- client/src/components/ContactIcons.jsx
+++ client/src/components/ContactIcons.jsx
@@ -1,8 +1,7 @@
+import Contact_Icon from "./Contact_Icon";
 import { dataDvoskinContact } from "../data/dataDvoskin/dataContact";
-import { FaGithub } from "react-icons/fa";
 import { BiLogoGmail } from "react-icons/bi";
-import { SiGmail } from "react-icons/si";
-import { BiLogoLinkedinSquare } from "react-icons/bi";
+import { FaGithub } from "react-icons/fa";
 import { FaLinkedinIn } from "react-icons/fa";
 import { SiWhatsapp } from "react-icons/si";
 
@@ -10,29 +9,17 @@ import { SiWhatsapp } from "react-icons/si";
 const ContactIcons = () => {
   const dataContact = dataDvoskinContact;
   const WHATSAPP_MESSAGE =
-    "Hi! I saw your portfolio. \nMy name is...";
+    "ENGLISH: Hi! I saw your portfolio. My name is...\n ESPAÑOL: Hola! He visto tu portfolio. Mi nombre es...";
   const encodedMessage = encodeURIComponent(WHATSAPP_MESSAGE);
   const WHATSAPP_URL = `https://wa.me/${dataContact.telephoneNumber}?text=${encodedMessage}`;
 
 
   return (
-    <section className="grid grid-cols-2 justify-center items-center dark:rounded-3xl lg:grid-cols-4">
-      <a href={dataContact.github} target="_blank">
-        <FaGithub size={60} className="text-gray-700 dark:text-gray-200"/>
-      </a>
-
-      <a href={`mailto:${dataContact.gmail}`} target="_blank">
-        <BiLogoGmail size={74} className="text-red-500"/>
-      </a>
-
-      <a href={dataContact.linkedin} target="_blank">
-        <FaLinkedinIn size={74} className="text-blue-800 dark:text-blue-400"/>
-      </a>
-
-      <a href={WHATSAPP_URL} target="_blank">
-        <SiWhatsapp size={60} className="text-green-600" />
-      </a>
-
+    <section className="flex justify-center items-center">
+      <Contact_Icon href={`mailto:${dataContact.gmail}`} Icon={BiLogoGmail} size={74} className="text-red-500" />
+      <Contact_Icon href={dataContact.github} Icon={FaGithub} size={60} className="text-gray-700 dark:text-gray-300" />
+      <Contact_Icon href={dataContact.linkedin} Icon={FaLinkedinIn} size={74} className="text-blue-800 dark:text-blue-400" />
+      <Contact_Icon href={WHATSAPP_URL} Icon={SiWhatsapp} size={60} className="text-green-600" />
     </section>
   );
 };
