32359c1840f1bc6863fb69bfd3908e484cbccedf
gastondvoskin <dvoskingaston@gmail.com>
2023-09-23 18:20:20 -0300

    Home responsive

--- client/src/components/SectionContainer.jsx
+++ client/src/components/SectionContainer.jsx
@@ -5,7 +5,7 @@ const SectionContainer = ({ name, bgNumber, children }) => {
       className={`flex flex-col w-full items-center pt-28 pb-28 bg-mybg${bgNumber} dark:bg-mybg${bgNumber}d relative`}
     >
         <div className="absolute inset-0 w-full h-20">
-            <div className={`transform skew-y-2 origin-top-right h-full w-full bg-mybg${bgNumber} dark:bg-mybg${bgNumber}d`}></div>
+            <div className={`transform skew-y-6 origin-top-right h-full w-full bg-mybg${bgNumber} dark:bg-mybg${bgNumber}d md:skew-y-2`}></div>
         </div>
         {children}
     </section>
