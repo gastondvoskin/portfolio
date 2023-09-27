5ce56b0004b626b523b752572e796198efafcb6e
gastondvoskin <dvoskingaston@gmail.com>
2023-09-27 08:52:47 -0300

    Added icons to buttons. Deleted old Technologies components

--- client/src/components/Button.jsx
+++ client/src/components/Button.jsx
@@ -1,11 +1,11 @@
-const Button = ({ parent, text, type, icon, onClickHandler }) => {
+const Button = ({ parent, text, type, Icon, onClickHandler }) => {
   return (
     <button
       onClick={onClickHandler}
       type={type}
       className={`flex items-center mt-6 mx-auto px-2 py-1 text-base text-center bg-myacc text-white hover:bg-myacchover rounded-sm duration-300 shadow-sm shadow-black dark:shadow-white lg:px-4 lg:py-2 lg:text-xl ${parent === "form" ? "mt-2 mb-3 lg:mb-0" : ""} dark:hover:bg-myaccdhover`} 
     >
-      {icon && <span className="mr-2">{icon}</span>}
+      {Icon && <span className="mr-2">{<Icon size={18} />}</span>}
       <span>{text}</span>
     </button>
   );
