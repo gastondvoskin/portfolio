0a257861556fe431c51250cdfec793b16ca6ceba
gastondvoskin <dvoskingaston@gmail.com>
2023-09-27 10:37:08 -0300

    Minor restyling in Technologies

--- client/src/components/Techs_Icon.jsx
+++ client/src/components/Techs_Icon.jsx
@@ -1,7 +1,7 @@
 const Techs_Icon = ({ name, Icon, className }) => {
   return (
     <div className="flex flex-col items-center">
-      <Icon size={60} className={className} />
+      <Icon size={75} className={className} />
       <h3 className="mt-1 lg:mt-2 text-sm lg:text-base">{name}</h3>
     </div>
   );
