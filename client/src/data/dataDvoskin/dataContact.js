f4252fd4be1b9b270343085b440bef0377369264
Gaston Dvoskin <dvoskingaston@gmail.com>
2024-07-12 12:19:05 -0300

    Updated Contact. Commented Whatsapp, added labels to Gmail, GitHub, LinkedIn

--- client/src/data/dataDvoskin/dataContact.js
+++ client/src/data/dataDvoskin/dataContact.js
@@ -2,6 +2,5 @@ const dataDvoskinContact = {
     github: "https://github.com/gastondvoskin/",
     linkedin:"https://www.linkedin.com/in/gaston-dvoskin/",
     gmail:"dvoskingaston@gmail.com",
-    telephoneNumber:"+5491156559234"
 }
 export { dataDvoskinContact };
