ce2be158c5467657c5c76f8820ba6d8d370c5a99
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-02-03 07:36:56 -0300

    Updated CV and CV anchors targets

--- client/src/sections/About.jsx
+++ client/src/sections/About.jsx
@@ -16,10 +16,10 @@ const About = () => {
             <p key={index} className="text-center mb-4 text-sm font-sans md:text-base">{paragraph}</p>
           )}
         </div>
-        <a href={cvDvoskin}>
+        <a href={cvDvoskin} target="_blank" rel="noreferrer">
           <Button text="VIEW CV" type="button" Icon={LuView} />
         </a>
-        <a href={cvDvoskin} download>
+        <a href={cvDvoskin} download target="_blank" rel="noreferrer">
           <Button text="DOWNLOAD CV" type="button" Icon={BiSolidDownload} />
         </a>
       </div>
