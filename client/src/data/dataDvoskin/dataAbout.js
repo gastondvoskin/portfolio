c78a9fbfb091ec79ffa107db5dc685b63ccf9a88
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-02-03 06:58:39 -0300

    Updated dataAbout.js text

--- client/src/data/dataDvoskin/dataAbout.js
+++ client/src/data/dataDvoskin/dataAbout.js
@@ -1,11 +1,8 @@
 const dataDvoskinAbout = [
-  "Hi! I'm a Front-end Developer with 4 years of experience, specialized in React, and with advanced proficiency in English (C2).", 
+  "Hi! I’m a Front-end Developer with 4 years of experience, specializing in React and an advanced proficiency in English (C1). Currently, I work as a Front-end Developer at ReadyMind, a Microsoft partner IT consultancy. Previously, I worked as a Wix Developer at Cuerdos Vocales and CODICU.", 
 
-  "I work as a Front-end Developer at ReadyMind. Previously, I worked as a Wix Developer at Cuerdos Vocales and CODICU.",  
-
-  "Additionally, I have 10 years of experience as Director of cultural projects, developing leadership skills.", 
+  "Before transitioning to tech,  I earned a degree in Choral Conducting and directed cultural projects, such as the Bicentennial Choir, for 10 years, developing leadership skills.", 
 ];
 
 export { dataDvoskinAbout };
-
 
