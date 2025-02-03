3efbdaa1b9c671de1e0c46eaec54ee2450658650
Gaston Dvoskin <dvoskingaston@gmail.com>
2025-02-03 07:15:25 -0300

    Added passions to dataAbout.js

--- client/src/data/dataDvoskin/dataAbout.js
+++ client/src/data/dataDvoskin/dataAbout.js
@@ -1,8 +1,10 @@
 const dataDvoskinAbout = [
   "Hi! I’m a Front-end Developer with 4 years of experience, specializing in React and an advanced proficiency in English (C1). Currently, I work as a Front-end Developer at ReadyMind, a Microsoft partner IT consultancy. Previously, I worked as a Wix Developer at Cuerdos Vocales and CODICU.", 
 
-  "Before transitioning to tech,  I earned a degree in Choral Conducting and directed cultural projects, such as the Bicentennial Choir, for 10 years, developing leadership skills.", 
+  "Beyond development, I have two passions: the arts and, more recently, AI. Before transitioning to tech, I earned a degree in Choral Conducting and directed cultural projects—such as the Bicentennial Choir—for 10 years, which honed my leadership skills. In the realm of AI, I integrate it into my daily work to boost efficiency, automate repetitive tasks, and explore new perspectives while continuously learning.", 
 ];
 
 export { dataDvoskinAbout };
+
+
 
